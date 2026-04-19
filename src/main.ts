3d2c47c40ec22e1e142a4192d30206389bb4144a
ZERO <d.elliotcab22@gmail.com>
2026-04-19 14:52:20 -0600

    feat: implement global exception filter for standardized error responses

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { GlobalExceptionFilter } from './common/filters/global-exception.filter';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
@@ -12,6 +13,10 @@ async function bootstrap() {
         forbidNonWhitelisted: true,
         transform: true,
       }),
+    );
+
+    app.useGlobalFilters(
+      new GlobalExceptionFilter()
     );
 
 
